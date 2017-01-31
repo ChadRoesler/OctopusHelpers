73afa46ca65a8763995e4470d042e5b11fc01033
Chad Roesler <ChadRoesler@Me.com>
2017-01-31 10:25:30 -0600

    Additional Tools
    Addition for TaskHelper and ActivitiyElementHelper.

---
+++ OctopusHelpers/ActivityElementHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Octopus.Client.Model;
+using OctopusHelpers.Constants;
+
+namespace OctopusHelpers
+{
+    public static class ActivityElementHelper
+    {
+        public static string GetLogInfo(ActivityElement activityElementToProcess, int tabIndex, List<ActivityStatus> statusesToCheck = null)
+        {
+            var output = string.Empty;
+            var tabCount = new string(' ', tabIndex * 5);
+            if (statusesToCheck == null)
+            {
+                output += activityElementToProcess.Name + ResourceStrings.Return;
+                foreach (var activityLogElement in activityElementToProcess.LogElements)
+                {
+                    output += (string.Format(ResourceStrings.ErrorPrinting, tabCount, activityLogElement.MessageText.Replace(ResourceStrings.Return, string.Format(ResourceStrings.ErrorPrinting, ResourceStrings.Return, tabCount)))) + ResourceStrings.Return;
+                }
+                foreach (var activityElement in activityElementToProcess.Children)
+                {
+                    output += GetLogInfo(activityElement, tabIndex + 1, statusesToCheck);
+                }
+            }
+            else
+            {
+                if (statusesToCheck.Contains(activityElementToProcess.Status))
+                {
+                    output += activityElementToProcess.Name + ResourceStrings.Return;
+                    foreach (var activityLogElement in activityElementToProcess.LogElements)
+                    {
+                        output += (string.Format(ResourceStrings.ErrorPrinting, tabCount, activityLogElement.MessageText.Replace(ResourceStrings.Return, string.Format(ResourceStrings.ErrorPrinting, ResourceStrings.Return, tabCount)))) + ResourceStrings.Return;
+                    }
+                    foreach (var activityElement in activityElementToProcess.Children)
+                    {
+                        output += GetLogInfo(activityElement, tabIndex + 1, statusesToCheck);
+                    }
+                }
+            }
+            return output.TrimEnd();
+        }
+    }
+}
---
+++ OctopusHelpers/TaskHelper.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Octopus.Client;
+using Octopus.Client.Model;
+using OctopusHelpers.Constants;
+
+namespace OctopusHelpers
+{
+    public static class TaskHelper
+    {
+        public static string GetErrors(OctopusRepository octRepository, TaskResource task)
+        {
+            var activitySteps = octRepository.Tasks.GetDetails(task).ActivityLogs.FirstOrDefault().Children;
+            var output = string.Empty;
+            var statusList = new List<ActivityStatus>
+            {
+                ActivityStatus.Failed
+            };
+            foreach (var activityStep in activitySteps.Where(a => a.Status == ActivityStatus.Failed))
+            {
+                output += ActivityElementHelper.GetLogInfo(activityStep, 0, statusList) + ResourceStrings.Return;
+            }
+            return output.Trim();
+        }
+
+        public static string GetWarnings(OctopusRepository octRepository, TaskResource task)
+        {
+            var activitySteps = octRepository.Tasks.GetDetails(task).ActivityLogs.FirstOrDefault().Children;
+            var output = string.Empty;
+            var statusList = new List<ActivityStatus>
+            {
+                ActivityStatus.SuccessWithWarning
+            };
+            foreach (var activityStep in activitySteps.Where(a => a.Status == ActivityStatus.SuccessWithWarning))
+            {
+                output += ActivityElementHelper.GetLogInfo(activityStep, 0, statusList) + ResourceStrings.Return;
+            }
+            return output.Trim();
+        }
+
+        public static string GetErrorsAndWarnings(OctopusRepository octRepository, TaskResource task)
+        {
+            var activitySteps = octRepository.Tasks.GetDetails(task).ActivityLogs.FirstOrDefault().Children;
+            var output = string.Empty;
+            var statusList = new List<ActivityStatus>
+            {
+                ActivityStatus.Failed,
+                ActivityStatus.SuccessWithWarning
+            };
+            foreach (var activityStep in activitySteps.Where(a => a.Status == ActivityStatus.Failed || a.Status == ActivityStatus.SuccessWithWarning))
+            {
+                output += ActivityElementHelper.GetLogInfo(activityStep, 0, statusList) + ResourceStrings.Return;
+            }
+            return output.Trim();
+        }
+
+        public static string GetFullLog(OctopusRepository octRepository, TaskResource task)
+        {
+            var activitySteps = octRepository.Tasks.GetDetails(task).ActivityLogs.FirstOrDefault().Children;
+            var output = string.Empty;
+            foreach (var activityStep in activitySteps)
+            {
+                output += ActivityElementHelper.GetLogInfo(activityStep, 0, null) + ResourceStrings.Return;
+            }
+            return output.Trim();
+        }
+    }
+}
